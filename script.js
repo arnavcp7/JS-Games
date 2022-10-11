01f809d0d5091a351c7d5ba4fb77d100f2f64680
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2022-10-12 00:10:20 +0530

    Advanced JS DOM Manipulation file
    Advanced JS DOM Manipulation file

--- script.js
+++ script.js
@@ -1,104 +1,125 @@
-let topRow = document.querySelector(".top-row");
-let leftCol = document.querySelector(".left-col");
-let topLeftCell = document.querySelector(".top-left-cell");
-let allCells = document.querySelectorAll(".cell");
-let addressInput = document.querySelector("#address");
-let formulaInput = document.querySelector("#formula");
-let lastSelectedCell;
+/*
+  Rock Paper Scissors 🚀🔥
+  Concepts covered in this project
+    👉 For loops
+    👉 Dom Manipulation
+    👉 Variables
+    👉 Conditionals (if else if)
+    👉 Template Literals
+    👉 Event Listeners
+    👉 Higher order Function (Math.random())
+*/
 
-cellsContentDiv.addEventListener("scroll",function(e){
-    let scrollFromTop = e.target.scrollTop;
-    let scrollFromLeft = e.target.scrollLeft;
-    topRow.style.top = scrollFromTop+"px";
-    leftCol.style.left = scrollFromLeft+"px";
-    topLeftCell.style.top = scrollFromTop+"px";
-    topLeftCell.style.left = scrollFromLeft+"px";
-})
+// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
+// getComputerChoice() 👉 'Rock'
+// getComputerChoice() 👉 'Scissors'
+function getComputerChoice() {
+  let choices = ['rock', 'paper', 'scissors'];
+  let result = choices[Math.floor(Math.random() * choices.length)];
+  return result;
+}
 
-let rowId;
-let colId;
-for(let i=0;i<allCells.length;i++){
-    allCells[i].addEventListener("click",function(e){
-        if(lastSelectedCell){
-            lastSelectedCell.classList.remove("active-cell");
-            document.querySelector(`div[trid='${colId}']`).classList.remove("active-row-col")
-            document.querySelector(`div[lcid='${rowId}']`).classList.remove("active-row-col");
-        }
-        
-        rowId = Number(e.target.getAttribute("rowid"));
-        colId = Number(e.target.getAttribute("colid"));
-        
-        e.target.classList.add("active-cell");
-        document.querySelector(`div[trid='${colId}']`).classList.add("active-row-col")
-        document.querySelector(`div[lcid='${rowId}']`).classList.add("active-row-col");
+// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
+// human wins - getResult('Rock', 'Scissors') 👉 1
+// human loses - getResult('Scissors', 'Rock') 👉 -1
+// human draws - getResult('Rock', 'Rock') 👉 0
+function getResult(playerChoice, computerChoice) {
+  // return the result of score based on if you won, drew, or lost
+  console.log(playerChoice + " and computerChoice : " + computerChoice);
+  if(playerChoice === computerChoice){
+    return 0;
+  }
 
-        let address = String.fromCharCode(65+colId)+(rowId+1)+"";
-        // console.log(address);
-        let cellObject = db[rowId][colId];
-        addressInput.value = address;
-        //update UI
-        formulaInput.value = cellObject.formula;
-        
-        cellObject.fontStyle.bold?document.querySelector(".bold").classList.add("active-font-style"):
-        document.querySelector(".bold").classList.remove("active-font-style");
+  // All situations where human draws, set `score` to 0
+  if(playerChoice == 'rock' &&  computerChoice == 'scissors')
+  {
+    return 1;
+  }
+  else if(playerChoice == 'scissors' && computerChoice == 'paper'){
+    return 1;
+  }
+  else if(playerChoice == 'paper' && computerChoice == 'rock'){
+    return 1;
+  }
+  else
+    return -1;
+  // All situations where human wins, set `score` to 1
+  // make sure to use else ifs here
+  
 
-        cellObject.fontStyle.italic?document.querySelector(".italic").classList.add("active-font-style"):
-        document.querySelector(".italic").classList.remove("active-font-style");
+  // Otherwise human loses (aka set score to -1)
+  
 
-        cellObject.fontStyle.underline?document.querySelector(".underline").classList.add("active-font-style"):
-        document.querySelector(".underline").classList.remove("active-font-style");
-    })
+  // return score
+  
+}
 
-    allCells[i].addEventListener("blur",function(e){
-        lastSelectedCell = e.target;
-        let cellValue = e.target.textContent;
-        let {rowId,colId} = getRowIdColIdFromElement(e.target);
-        let cellObject = db[rowId][colId];
-        if(cellObject.value == cellValue){
-            return;
-        }
-        cellObject.value = cellValue;
-        console.log("After UPdate",cellObject);
-        updateChildren(cellObject);
-        if(cellObject.visited){
-            return;
-        }
-        cellObject.visited = true;
-        visitedCells.push({"rowId":rowId,"colId":colId});
-    })
+// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+function showResult(score, playerChoice, computerChoice) {
+  // Hint: on a score of -1
+  // You should do result.innerText = 'You Lose!'
+  // Don't forget to grab the div with the 'result' id!
+  let answer = score;
+  let scoreDiv = document.getElementById('player-score');
+  if(scoreDiv == ""){
+    scoreDiv.innerText = answer;
+  }
+  else{
+    scoreDiv.innerText = Number(scoreDiv.innerText) + answer;
+  }
 
-    allCells[i].addEventListener("keydown",function(e){
-        if(e.key == 'Backspace'){
-            let cell = e.target;
-            let {rowId,colId} = getRowIdColIdFromElement(cell);
-            let cellObject = db[rowId][colId];
-            if(cellObject.formula){
-                //update db
-                cell.formula = "";
-                //update ui 
-                formulaInput.value = "";
-                cell.textContent = "";
-                removeFormula(cellObject);
-            }
-        }
-    })
+  let result = document.getElementById('result');
+  if(answer == -1){
+    result.innerText = "You lose!";
+  }
+  else if(answer == 1){
+    result.innerText = "You win!";
+  }
+  else{
+    result.innerText = "Drawn!";
+  }
+}
+
+// ** Calculate who won and show it on the screen **
+function onClickRPS(playerChoice) {
+  let computerChoice = getComputerChoice();
+  let score =  getResult(playerChoice, computerChoice);
+  showResult(score, playerChoice, computerChoice);
 }
 
 
-formulaInput.addEventListener("blur",function(e){
-    let formula = e.target.value;
-    if(formula){
-        let {rowId,colId} = getRowIdColIdFromElement(lastSelectedCell);
-        let cellObject = db[rowId][colId];
-        if(cellObject.formula){
-            removeFormula(cellObject);
-        }
-        let computedValue = solveFormula(formula,cellObject); // will implement in next commit
-        //update db
-        cellObject.value = computedValue;
-        cellObject.formula = formula;
-        //update ui
-        lastSelectedCell.textContent = computedValue;
-        updateChildren(cellObject);
+// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+function playGame() {
+    console.log("Game started");
+  // use querySelector to select all RPS Buttons
+  let rpsS = document.querySelectorAll('.rpsButton');
+  // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
+  rpsS.forEach(rps => {
+    rps.onclick = () => {
+        let playerChoice = rps.value;
+        onClickRPS(playerChoice);
     }
-})
+  })
+  // 1. loop through the buttons using a forEach loop
+  // 2. Add a 'click' event listener to each button
+  // 3. Call the onClickRPS function every time someone clicks
+  // 4. Make sure to pass the currently selected rps button as an argument
+
+ 
+  let endclick = document.getElementById('endGameButton');
+  // Add a click listener to the end game button that runs the endGame() function on click
+  endclick.onclick = () => {
+    endGame();
+  }
+}
+
+// ** endGame function clears all the text on the DOM **
+function endGame() {
+    let result = document.getElementById('result');
+    result.innerHTML = '';
+    let scoreDiv = document.getElementById('player-score');
+    scoreDiv.innerHTML = '';
+
+}
+
+playGame()
